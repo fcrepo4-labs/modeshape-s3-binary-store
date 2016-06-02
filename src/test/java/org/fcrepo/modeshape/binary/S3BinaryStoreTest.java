75082aff03ad2c5decf62517514e522da098cfe0
Bill Branan <bbranan@duraspace.org>
2016-06-02 16:36:52 -0400

    Updates code package

--- src/test/java/org/fcrepo/modeshape/binary/S3BinaryStoreTest.java
+++ src/test/java/org/fcrepo/modeshape/binary/S3BinaryStoreTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.fcrepo;
+package org.fcrepo.modeshape.binary;
 
 import java.io.IOException;
 import java.io.InputStream;
